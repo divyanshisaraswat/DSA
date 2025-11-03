e8ef31ebbdcff1dd0553c5ed52cceffe51f3cf90
divyanshisaraswat <divyasaraswat924@gmail.com>
2025-11-03 22:51:37 +0530

    gas-station_circular-tour.cpp

---
+++ gas-station_circular-tour.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        int deficit=0;
+        int balance=0;
+        int start=0;
+        int n=gas.size();
+        
+        for(int i=0;i<n;i++){
+            balance+= gas[i]-cost[i];
+            if(balance <0){
+                deficit+= balance;
+                start = i+1;
+                balance=0;
+            }
+        }
+        
+        if(deficit + balance >=0){
+            return start;
+        }
+            else {
+                return -1;
+            }
+        
+    }
+};
